8b8457023a752952579c34d62f61ca3d65d70772
Siddhant Mishra <siddhant48311@gmail.com>
2020-07-17 15:49:35 +0530

    Updates

---
+++ precache-manifest.dce9aa959642846d31da28725bc5ed53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cc79f0e7dfa35f5a942aefc1d91ee8",
+    "url": "/Corona-Tracker/index.html"
+  },
+  {
+    "revision": "95051ce6c6585ad42a29",
+    "url": "/Corona-Tracker/static/css/main.d3b52005.chunk.css"
+  },
+  {
+    "revision": "f6f9d646f64458208259",
+    "url": "/Corona-Tracker/static/js/2.b86c90ef.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-Tracker/static/js/2.b86c90ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95051ce6c6585ad42a29",
+    "url": "/Corona-Tracker/static/js/main.3d9d5158.chunk.js"
+  },
+  {
+    "revision": "c989b080ae038775e5fd",
+    "url": "/Corona-Tracker/static/js/runtime-main.c3c54099.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona-Tracker/static/media/image.d7265326.png"
+  }
+]);
